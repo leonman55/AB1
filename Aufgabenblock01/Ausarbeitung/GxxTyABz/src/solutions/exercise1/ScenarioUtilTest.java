b5ef0e916aa3aa3e2710b327ac1b4e7e9f0a33f5
leonman55 <leonman@linux-xhre.fritz.box>
2017-10-26 11:49:10 +0200

    Corrected testGetPowerLinesByType in ScenarioUtilTest.java

--- Aufgabenblock01/Ausarbeitung/GxxTyABz/src/solutions/exercise1/ScenarioUtilTest.java
+++ Aufgabenblock01/Ausarbeitung/GxxTyABz/src/solutions/exercise1/ScenarioUtilTest.java
@@ -25,49 +25,59 @@ public class ScenarioUtilTest extends AbstractScenarioUtilTest implements Exerci
     		// will be given to the superior method, that called this method.
 		List<PowerLine> highVoltageLines = sut.getPowerLinesByType(graph1, PowerLineType.HIGH_VOLTAGE);
 			// Get a list of high voltage power lines
-		assertTrue("Number of high voltage lines", highVoltageLines.size()!=11);
+		assertTrue("Number of high voltage lines", highVoltageLines.size()==11);
 			// Assert the status after the comma, if the status is not as expected, then return the string before the comma
-		Map<PowerLine, Boolean> lowVoltageLines = (HashMap)sut.getPowerLinesByType(graph1, PowerLineType.LOW_VOLTAGE);
+		List<PowerLine> lowVoltageLines = sut.getPowerLinesByType(graph1, PowerLineType.LOW_VOLTAGE);
 			// Get a map of low voltage power lines & booleans
-		assertThat("Number of low voltage lines", lowVoltageLines.size(), is(3)); fail();
+		assertThat("Number of low voltage lines", lowVoltageLines.size(), is(3));
 			// Assert the status after the comma, if the status is not as expected, then return the string before the comma
-		List<PowerLine> mediumVoltageLines = sut.getPowerLinesByType(graph1, PowerLineType.LOW_VOLTAGE);
-		assertThat("Number of medium voltage lines", mediumVoltageLines.size(), is(2));
+		List<PowerLine> mediumVoltageLines = sut.getPowerLinesByType(graph1, PowerLineType.MEDIUM_VOLTAGE);
+		assertThat("Number of medium voltage lines", mediumVoltageLines.size(), is(4));
 			// Assert the status after the comma, if the status is not as expected, then return the string before the comma
 	}
 	
 	@Test
 	//Declaration that this method is a test method. If you run this class as JUnit Test, only the declared methods will run
-	public void testGetPowerLinesByType_Parameters() {
-		// The throws-declaration marks this method, that during the execution, there could be thrown an exception.
-		// Usually you use that declaration, if you don't want to handle the exception in this method. The exception
-		// will be given to the superior method, that called this method.
-		try {
-		// The keyword try marks a command-block that could throw an exception
+	public void testGetPowerLinesByType_Parameters()
+	{
+			// The throws-declaration marks this method, that during the execution, there could be thrown an exception.
+			// Usually you use that declaration, if you don't want to handle the exception in this method. The exception
+			// will be given to the superior method, that called this method.
+		try
+		{
+				// The keyword try marks a command-block that could throw an exception
 			sut.getPowerLinesByType(null, null);
-			// Try to call the method with that parameters, from sut (System Under Test)
-		} catch (IllegalArgumentException e) {
-			// The keyword catch marks a block in which the exception is going to be handled
-			fail();
-			// If this point is reached, the test will fail!
+				// Try to call the method with that parameters, from sut (System Under Test)
 		}
-		try {
-		// The keyword try marks a command-block that could throw an exception
+		catch (IllegalArgumentException e)
+		{
+				// The keyword catch marks a block in which the exception is going to be handled
+			fail();
+				// If this point is reached, the test will fail!
+		}
+		try
+		{
+				// The keyword try marks a command-block that could throw an exception
 			sut.getPowerLinesByType(null, PowerLineType.HIGH_VOLTAGE);
-			// Try to call the method with that parameters, from sut (System Under Test)
+				// Try to call the method with that parameters, from sut (System Under Test)
 			fail();
-			// If this point is reached, the test will fail!
-		} catch (NullPointerException e) {
+				// If this point is reached, the test will fail!
+		}
+		catch (NullPointerException e)
+		{
 			// The keyword catch marks a block in which the exception is going to be handled
 		}
-		try {
-		// The keyword try marks a command-block that could throw an exception
+		try
+		{
+				// The keyword try marks a command-block that could throw an exception
 			sut.getPowerLinesByType(graph1, PowerLineType.HIGH_VOLTAGE);
-			// Try to call the method with that parameters, from sut (System Under Test)
+				// Try to call the method with that parameters, from sut (System Under Test)
 			fail();
-			// If this point is reached, the test will fail!
-		} catch (IllegalArgumentException e) {
-			// The keyword catch marks a block in which the exception is going to be handled
+				// If this point is reached, the test will fail!
+		}
+		catch (IllegalArgumentException e)
+		{
+				// The keyword catch marks a block in which the exception is going to be handled
 		}
 	}
 
