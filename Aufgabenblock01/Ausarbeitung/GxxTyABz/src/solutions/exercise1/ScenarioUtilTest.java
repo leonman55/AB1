2722195bc7602f7c15dcc3c13b36ad5e7456c3c5
leonman55 <leonman@linux-xhre.fritz.box>
2017-10-26 12:07:06 +0200

    Corrected testGetPowerLinesByType_Parameters in ScenarioUtilTest Comments missing

--- Aufgabenblock01/Ausarbeitung/GxxTyABz/src/solutions/exercise1/ScenarioUtilTest.java
+++ Aufgabenblock01/Ausarbeitung/GxxTyABz/src/solutions/exercise1/ScenarioUtilTest.java
@@ -38,7 +38,7 @@ public class ScenarioUtilTest extends AbstractScenarioUtilTest implements Exerci
 	
 	@Test
 	//Declaration that this method is a test method. If you run this class as JUnit Test, only the declared methods will run
-	public void testGetPowerLinesByType_Parameters()
+	public void testGetPowerLinesByType_Parameters() 
 	{
 			// The throws-declaration marks this method, that during the execution, there could be thrown an exception.
 			// Usually you use that declaration, if you don't want to handle the exception in this method. The exception
@@ -47,12 +47,12 @@ public class ScenarioUtilTest extends AbstractScenarioUtilTest implements Exerci
 		{
 				// The keyword try marks a command-block that could throw an exception
 			sut.getPowerLinesByType(null, null);
+			fail();
 				// Try to call the method with that parameters, from sut (System Under Test)
 		}
 		catch (IllegalArgumentException e)
 		{
 				// The keyword catch marks a block in which the exception is going to be handled
-			fail();
 				// If this point is reached, the test will fail!
 		}
 		try
@@ -63,7 +63,7 @@ public class ScenarioUtilTest extends AbstractScenarioUtilTest implements Exerci
 			fail();
 				// If this point is reached, the test will fail!
 		}
-		catch (NullPointerException e)
+		catch (IllegalArgumentException e)
 		{
 			// The keyword catch marks a block in which the exception is going to be handled
 		}
@@ -72,7 +72,6 @@ public class ScenarioUtilTest extends AbstractScenarioUtilTest implements Exerci
 				// The keyword try marks a command-block that could throw an exception
 			sut.getPowerLinesByType(graph1, PowerLineType.HIGH_VOLTAGE);
 				// Try to call the method with that parameters, from sut (System Under Test)
-			fail();
 				// If this point is reached, the test will fail!
 		}
 		catch (IllegalArgumentException e)
