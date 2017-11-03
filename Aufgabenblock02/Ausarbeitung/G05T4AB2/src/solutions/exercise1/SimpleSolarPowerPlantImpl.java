287fd7b9a6d77439006dbaaa67b0a9d7b38e03a1
leonman55 <>
2017-11-03 15:30:49 +0100

    Added Aufgabenblock02

---
+++ Aufgabenblock02/Ausarbeitung/G05T4AB2/src/solutions/exercise1/SimpleSolarPowerPlantImpl.java
@@ -0,0 +1,34 @@
+package solutions.exercise1;
+
+import org.sopra.api.exercises.ExerciseSubmission;
+import org.sopra.api.exercises.exercise1.SimpleProducer;
+
+public class SimpleSolarPowerPlantImpl extends SimpleEnergyNodeImpl implements ExerciseSubmission, SimpleProducer {
+
+	/**
+	 * Constructor of a SolarPowerPlantImpl instance.
+	 * 
+	 * @param x Integer
+	 * @param y Integer
+	 */
+	public SimpleSolarPowerPlantImpl(int x, int y) {
+		super(x, y);
+	}
+
+	/**
+	 * @return the return value of the super type method + a {@link String} of the form ": (Type of this PowerPlant)"
+	 */
+	@Override
+	public String getProducerName() {
+		return super.getName(); // + ": SolarPowerPlant";
+	}
+
+	/**
+	 * @see org.sopra.api.exercises.ExerciseSubmission#getTeamIdentifier()
+	 */
+	@Override
+	public String getTeamIdentifier() {
+		return "Musterloesung";
+	}
+
+}
---
+++ Aufgabenblock02/GxxTyAB2/src/solutions/exercise2/QuicksortTest.java
@@ -0,0 +1,38 @@
+package solutions.exercise2;
+
+import org.sopra.api.exercises.ExerciseSubmission;
+import org.sopra.api.exercises.exercise2.AbstractQuicksortTest;
+
+public class QuicksortTest extends AbstractQuicksortTest implements ExerciseSubmission{
+
+    @Override
+    public void testPartition() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void testPartition_Parameters() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void testQuicksort() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void testQuicksort_Parameters() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getTeamIdentifier() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
