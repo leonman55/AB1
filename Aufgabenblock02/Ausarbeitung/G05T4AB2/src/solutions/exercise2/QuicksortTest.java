4aec23be2b8e61778e65eab32439f6b1b10a7725
Fossi42 <Blablabla@trash-mail.com>
2017-11-04 13:03:16 +0100

    2.3 und 2.4 angefangen

--- Aufgabenblock02/Ausarbeitung/G05T4AB2/src/solutions/exercise2/QuicksortTest.java
+++ Aufgabenblock02/Ausarbeitung/G05T4AB2/src/solutions/exercise2/QuicksortTest.java
@@ -1,38 +1,85 @@
 package solutions.exercise2;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.sopra.api.exercises.ExerciseSubmission;
 import org.sopra.api.exercises.exercise2.AbstractQuicksortTest;
 
-public class QuicksortTest extends AbstractQuicksortTest implements ExerciseSubmission{
+public class QuicksortTest extends AbstractQuicksortTest implements ExerciseSubmission {
 
-    @Override
-    public void testPartition() {
-	// TODO Auto-generated method stub
+	@Test
+	public void testPartition() {
+		Integer[] arr1 = { 1, 1, 1, 1, 1 };
 
-    }
+		int treffpunkt = sut.partition(arr1, 0, 1);
+		assertEquals(treffpunkt, 1);
 
-    @Override
-    public void testPartition_Parameters() {
-	// TODO Auto-generated method stub
+		treffpunkt = sut.partition(arr1, 0, 2);
+		assertEquals(treffpunkt, 2);
 
-    }
+		treffpunkt = sut.partition(arr1, 0, 3);
+		assertEquals(treffpunkt, 2);
 
-    @Override
-    public void testQuicksort() {
-	// TODO Auto-generated method stub
+		treffpunkt = sut.partition(arr1, 0, 4);
+		assertEquals(treffpunkt, 3);
 
-    }
+		Integer[] arr2 = { 8, 3, 6, 4, 1 };
+		treffpunkt = sut.partition(arr2, 0, 4);
 
-    @Override
-    public void testQuicksort_Parameters() {
-	// TODO Auto-generated method stub
+		for (int i = 0; i <= treffpunkt; i++) {
+			assertTrue(arr2[treffpunkt] >= arr2[i]);
+		}
+		for (int j = 4; j >= treffpunkt; j--) {
+			assertTrue(arr2[treffpunkt] <= arr2[j]);
+		}
+	}
 
-    }
+	@Test
+	public void testPartition_Parameters() {
+		Integer[] arr = { 1, 2, 3 };
+		try {
+			sut.partition(null, 0, 1);
+			fail();
+		} catch (IllegalArgumentException e) {
 
-    @Override
-    public String getTeamIdentifier() {
-	// TODO Auto-generated method stub
-	return null;
-    }
+		}
+		try {
+			sut.partition(arr, -1, 2);
+			fail();
+		} catch (IllegalArgumentException e) {
+
+		}
+		try {
+			sut.partition(arr, 0, -1);
+			fail();
+		} catch (IllegalArgumentException e) {
+
+		}
+		try {
+			sut.partition(arr, -3, -1);
+			fail();
+		} catch (IllegalArgumentException e) {
+
+		}
+
+	}
+
+	@Override
+	public void testQuicksort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void testQuicksort_Parameters() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getTeamIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
