d0b1fd207d5346594063e3cb135798d2b71c3ebc
leonman55 <leonman@linux-xhre.fritz.box>
2017-11-08 14:26:26 +0100

    Added Fossis implementation of QuickSortImpl

---
+++ Aufgabenblock02/Ausarbeitung/G05T4AB2/src/solutions/exercise2/QuicksortImpl.java
@@ -0,0 +1,103 @@
+package solutions.exercise2;
+
+//import java.lang.reflect.Array;
+import java.util.Comparator;
+
+import org.sopra.api.exercises.ExerciseSubmission;
+import org.sopra.api.exercises.exercise2.Quicksort;
+
+/**
+ * Implementation of the Quicksort algorithm
+ * 
+ * @author Isabelle, Leon, Pascal, Stefan
+ */
+public class QuicksortImpl<T> implements Quicksort<T>, ExerciseSubmission {
+
+	private final Comparator<T> comparator;
+
+	public QuicksortImpl(Comparator<T> comparator) throws IllegalArgumentException {
+		if (comparator == null) {
+			throw new IllegalArgumentException("Parameter is not allowed to be null");
+		}
+		this.comparator = comparator;
+	}
+
+	/**
+	 * Aufteilung des Arguments in zwei Gruppen. Die erste beinhaltet Werte die
+	 * kleiner(/gleich) als das mittlere Element und die zweite Werte
+	 * groesser(/gleich) dem mittleren Element.
+	 * 
+	 * @param
+	 * @return
+	 */
+	@Override
+	public int partition(T[] arr, int left, int right) {
+		// TODO Noch mehr zu prüfen?
+		if (arr == null || left > right || left < 0 || right < 0 || right > arr.length - 1 || left > arr.length - 1) {
+			throw new IllegalArgumentException("Wrong Parameter(s)");
+		}
+		// Pivo Element in die Mitte setzen
+		T pivo = arr[(left + right) / 2];
+		// Index der von links hoch läuft
+		int i = left;
+		// Index der von recht runter läuft
+		int j = right;
+		// Ein Platzhalter für den tausch
+		T platzhalter;
+
+		do {
+			// Solange von Links und Rechts zum Pivo Element laufen bis Werte gefunden
+			// wurden, die getauscht werden
+			while (comparator.compare(arr[i], pivo) < 0) {
+				i++;
+			}
+			while (comparator.compare(arr[j], pivo) > 0) {
+				j--;
+			}
+
+			// Elemente tauschen, falls sich der linke und rechte Zeiger noch nicht
+			// getroffen haben
+			if (i <= j) {
+				platzhalter = arr[i];
+				arr[i] = arr[j];
+				arr[j] = platzhalter;
+				i++;
+				j--;
+			}
+
+		} while (i <= j); // Solange tauschen bis sich die Zeiger treffen
+
+		// Der Index des Elements bei dem sich die Zeiger getroffen haben
+		return i;
+	}
+
+	/**
+	 * Sortieren der Argumente
+	 * 
+	 * @param
+	 */
+	@Override
+	public void quicksort(T[] arr, int left, int right) {
+		if (arr == null) {
+			throw new IllegalArgumentException("Parameter is not allowed to be null");
+		}
+
+		if (left < right) {
+			int treffpunkt = partition(arr, left, right);
+			if (left < treffpunkt) {
+				quicksort(arr, left, treffpunkt - 1);
+			}
+			if (right > treffpunkt) {
+				quicksort(arr, treffpunkt, right);
+			}
+		}
+
+	}
+
+	@Override
+	public String getTeamIdentifier() {
+		// TODO Auto-generated method stub
+		return "G05T04";
+	}
+
+}
