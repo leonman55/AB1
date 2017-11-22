9168b87bee60b05d208994c85b556b3c2001130f
leonman55 <leonman@linux-xhre.fritz.box>
2017-11-22 14:10:47 +0100

    Added Aufgabenblock03

--- Aufgabenblock02/Ausarbeitung/G05T4AB2/src/solutions/exercise2/QuicksortTest.java
+++ Aufgabenblock02/Ausarbeitung/G05T4AB2/src/solutions/exercise2/QuicksortTest.java
@@ -1,38 +1,56 @@
 package solutions.exercise2;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 import org.sopra.api.exercises.ExerciseSubmission;
 import org.sopra.api.exercises.exercise2.AbstractQuicksortTest;
 
-public class QuicksortTest extends AbstractQuicksortTest implements ExerciseSubmission{
+/**
+ * @author Isabelle, Leon, Pascal, Stefan
+ */
+public class QuicksortTest extends AbstractQuicksortTest implements ExerciseSubmission
+{
+	Integer[] testint = {5, 8, 1, 5, 76, 3, 8, 9, 10};
+	
+	@Override
+	@Test
+	public void testPartition()
+	{
+		// TODO Auto-generated method stub
+		int back = sut.partition(testint, 0, 8);
+		System.out.println(back);
+		assertEquals(back, 76);
+	}
 
-    @Override
-    public void testPartition() {
-	// TODO Auto-generated method stub
+	@Override
+	@Test
+	public void testPartition_Parameters()
+	{
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void testPartition_Parameters() {
-	// TODO Auto-generated method stub
+	@Override
+	@Test
+	public void testQuicksort()
+	{
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void testQuicksort() {
-	// TODO Auto-generated method stub
+	@Override
+	@Test
+	public void testQuicksort_Parameters()
+	{
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void testQuicksort_Parameters() {
-	// TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public String getTeamIdentifier() {
-	// TODO Auto-generated method stub
-	return null;
-    }
+	@Override
+	public String getTeamIdentifier()
+	{
+		return "G05T04";
+	}
 
 }
