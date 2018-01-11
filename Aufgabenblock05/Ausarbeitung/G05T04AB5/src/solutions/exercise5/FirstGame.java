d7d3f1a428d5e3eae9a36b9a14405d2765d54a32
Fossi42 <Blablabla@trash-mail.com>
2018-01-11 20:10:18 +0100

    5.3 fertig ohne Kommentare

--- Aufgabenblock05/Ausarbeitung/G05T04AB5/src/solutions/exercise5/FirstGame.java
+++ Aufgabenblock05/Ausarbeitung/G05T04AB5/src/solutions/exercise5/FirstGame.java
@@ -1,42 +1,44 @@
 package solutions.exercise5;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-//import java.util.Arrays;
-//import java.util.Collections;
 import java.util.List;
-
-//import java.util.HashSet;
-//import java.util.Set;
 
 import org.sopra.api.Game;
 import org.sopra.api.Scenario;
 import org.sopra.api.UpgradePowerLineCostCalculator;
 import org.sopra.api.exercises.ExerciseSubmission;
-//import org.sopra.api.command.BuildPowerPlantCommand;
 import org.sopra.api.command.CannotExecuteCommandException;
-// import org.sopra.api.model.EnergyNode;
 import org.sopra.api.model.PlantLocation;
 import org.sopra.api.model.PlayfieldElement;
 import org.sopra.api.model.PowerLine;
 import org.sopra.api.model.PowerLineType;
-//import org.sopra.api.model.TransformerStation;
 import org.sopra.api.model.producer.ProducerType;
 
 import solutions.exercise2.ConstructionCostCalculatorImpl;
 import solutions.exercise2.PlayfieldElementComparator;
 import solutions.exercise2.QuicksortImpl;
 
+/**
+ * @author Isabelle, Leon, Pascal, Stefan
+ */
 public class FirstGame implements Game, ExerciseSubmission
 {
 
+	/**
+	 * returns the specific identification phrase for the team
+	 * @return team identification String
+	 */
 	@Override
 	public String getTeamIdentifier()
 	{
 		return "G05T04";
 	}
 
+	/**
+	 * Trys to build WIND_POWER_PLANT PlayfieldElement objects onto transformer stations and upgrade PowerLine objects to type HIGH_VOLTAGE bevore scenario starts
+	 * @param scenario the scenario corresponding to the current playfield
+	 * @throws CannotExecuteCommandException if something went wrong during upgrade or build method
+	*/
 	@Override
 	public void buildPhase(Scenario scenario) throws RuntimeException
 	{
@@ -46,42 +48,46 @@ public class FirstGame implements Game, ExerciseSubmission
 		/* Windkraftwerke bauen */
 		/************************/
 
-		// Alle Trafos finden
+		//find all energy node locations
 		List<PlantLocation> locations = scenario.getPlantLocations();
 		List<PlayfieldElement> transformer = new ArrayList<PlayfieldElement>();
+		//Goes through every plant location found
 		for (PlantLocation plantLocation : locations)
 		{
+			//Searches for transformer station PlayfieldElement objects
 			if (!plantLocation.isBuilt())
 			{
 				transformer.add(plantLocation.getPlayfieldElement());
 			}
 		}
 
-		// Weniger als 2 Baumöglichkeiten
+		//if there are less then two transformer locations left throw an RuntimeException
 		if (transformer.size() < 2)
 		{
 			throw new RuntimeException();
 		}
 
-		// günstigsten Preis für Windkraftanlagen finden
+		//Convert ArrayList of transformer stations to Array
 		PlayfieldElement[] transformerarr = transformer.toArray(new PlayfieldElement[transformer.size()]);
 
+		//New Comparator for WIND_POWER_PLANT PlayfieldElement objects
 		PlayfieldElementComparator comptransformer = new PlayfieldElementComparator(ProducerType.WIND_POWER_PLANT,
 				scenario);
+		//Quicksort for WIND_POWER_PLANT Comparator
 		QuicksortImpl<PlayfieldElement> sorttransformer = new QuicksortImpl<PlayfieldElement>(comptransformer);
+		//Execution for Quicksort algorithm
 		sorttransformer.quicksort(transformerarr, 0, transformerarr.length - 1);
 
-		// bauen
 		ConstructionCostCalculatorImpl costcalc = new ConstructionCostCalculatorImpl(scenario);
 		PlantLocation plantlocation = null;
 
 		for (int i = transformerarr.length - 1; i > transformerarr.length - 3; i--)
 		{
-			// prüfen ob noch genug budget vorhanden ist
+			//calculate, if budget is sufficient
 			if (cost + costcalc.calculateCost(transformerarr[i], ProducerType.WIND_POWER_PLANT) <= 20000)
 			{
-				// plantLocation suchen
 
+				//if budget fits search for location
 				for (PlantLocation plantloc : scenario.getPlantLocations())
 				{
 					if (plantloc.getPlayfieldElement().equals(transformerarr[i]))
@@ -90,39 +96,26 @@ public class FirstGame implements Game, ExerciseSubmission
 					}
 				}
 
+				//if a location was found try to build 
 				if (plantlocation != null)
 				{
 					try
 					{
-						// bau ausfuehren
+						//build plant
 						scenario.getCommandFactory()
 								.createBuildPlantCommand(plantlocation, ProducerType.WIND_POWER_PLANT).execute();
 						cost = cost + costcalc.calculateCost(transformerarr[i], ProducerType.WIND_POWER_PLANT);
 
 					}
+					//if an error occurs throw ConnotEcecuteCommandException
 					catch (CannotExecuteCommandException e)
 					{
 
 					}
 
 				}
-				else
-				{
-					// System.out.println("Plantlocation ist null");
-				}
-			}
-			else
-			{
-				// System.out.println("Baukosten ueberschritten");
 			}
 		}
-
-		// for (PlayfieldElement playfieldElement : transformerarr)
-		// {
-		// System.out.println(playfieldElement.getElementType());
-		// }
-		// System.out.println("-------------");
-		// System.out.println("_______________________________");
 
 		/************************/
 		/* Ausbau der Leitungen */
@@ -130,7 +123,7 @@ public class FirstGame implements Game, ExerciseSubmission
 
 		List<PowerLine> powerlines = new ArrayList<>();
 
-		// Alle Powerlines finden, die nicht ausgebaut sind
+		//search for all PowerLine object, which are not of type HIGH_Voltage
 		for (PowerLine powerline : scenario.getGraph().getEdges())
 		{
 			if (powerline.getType() != PowerLineType.HIGH_VOLTAGE)
@@ -139,22 +132,29 @@ public class FirstGame implements Game, ExerciseSubmission
 			}
 		}
 
+		//new CostCalculator for PowerLine objects
 		UpgradePowerLineCostCalculator costcalcpowerline = scenario.getCostCalculatorFactory()
 				.createUpgradePowerLineCostCalculator();
 
+		//go through all found non HIGH_VOLTAGE PowerLine objects
 		for (PowerLine powerLine : powerlines)
 		{
+			//If cost does not exceed 20000 try to upgrade PowerLine object to type HIGH_VOLTAGE
 			if (cost + costcalcpowerline.calculateCost(powerLine, scenario.getPlayfield(),
 					PowerLineType.HIGH_VOLTAGE) <= 20000)
 			{
 				try
 				{
+					//upgrade PowerLine to type HIGH_VOLTAGE
 					scenario.getCommandFactory().createUpgradeLineCommand(powerLine, PowerLineType.HIGH_VOLTAGE)
 							.execute();
-					;
+					
+					//update cost variable
 					cost = cost + costcalcpowerline.calculateCost(powerLine, scenario.getPlayfield(),
 							PowerLineType.HIGH_VOLTAGE);
 				}
+				
+				//if an error occurs throw ConnotEcecuteCommandException
 				catch (CannotExecuteCommandException e)
 				{
 
