972f4ec666331082cc8d5c0b9cfe8cdc9f787030
leonman55 <leonman@linux-xhre.fritz.box>
2018-01-18 15:02:13 +0100

    Changed Framework version and working implementation of ScenarioImpl

---
+++ Aufgabenblock06/Ausarbeitung/G05T4AB6/src/solutions/exercise5/SunEnergyBroker.java
@@ -0,0 +1,129 @@
+package solutions.exercise5;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.sopra.api.Scenario;
+import org.sopra.api.command.CannotAssignCommandException;
+import org.sopra.api.exercises.ExerciseSubmission;
+import org.sopra.api.exercises.exercise5.AbstractEnergyNetworkAnalyzer;
+import org.sopra.api.exercises.exercise5.AbstractSunEnergyBroker;
+import org.sopra.api.model.EnergyNode;
+import org.sopra.api.model.Graph;
+import org.sopra.api.model.PowerLine;
+import org.sopra.api.model.consumer.Consumer;
+import org.sopra.api.model.consumer.IndustrialPark;
+import org.sopra.api.model.producer.Producer;
+import org.sopra.api.model.producer.SolarPowerPlant;
+
+public class SunEnergyBroker extends AbstractSunEnergyBroker implements ExerciseSubmission {
+
+	private double[] sunIntensity;
+	private Map<Integer, Map<Consumer, Integer>> cached = new HashMap<>();
+	private Logger logger = LogManager.getLogger();
+
+	/**
+	 * Execution for given scenario in given round
+	 * 
+	 * @param scenario {@link Scenario}
+	 * @param round Integer
+	 */
+	@Override
+	public void executionPhase(Scenario scenario, int round) {
+		sunIntensity = scenario.getStatistics().getSunIntensityPerDay();
+
+		List<IndustrialPark> parks = scenario.getGraph().getNodes().stream().filter(n -> n instanceof IndustrialPark)
+				.map(n -> (IndustrialPark) n).collect(Collectors.toList());
+
+		if (round == 0) {
+			preProcess(scenario);
+
+			for (IndustrialPark p : parks) {
+				int amount = p.getRequiredPower();
+				try {
+					scenario.getCommandFactory().createAdjustConsumerCommand(p, -amount).assign();
+				} catch (CannotAssignCommandException e) {
+					e.printStackTrace();
+					throw new RuntimeException("Error in round " + round + " not expected to happen.");
+				}
+			}
+		}
+
+		for (IndustrialPark park : parks) {
+			int hour = round % 24;
+
+			int forcastIndex;
+			if (hour + park.getAdjustmentTime() - 1 < sunIntensity.length) {
+				forcastIndex = hour + park.getAdjustmentTime() - 1;
+			} else {
+				forcastIndex = hour + park.getAdjustmentTime() - 1 - sunIntensity.length;
+			}
+			logger.debug("Sun intensity at " + hour + ": " + sunIntensity[hour] + " --- forcasted sun intensity at "
+					+ forcastIndex + ": " + sunIntensity[forcastIndex]);
+
+			if (sunIntensity[hour] != sunIntensity[forcastIndex] && !park.isAdjusting()) {
+				int amount = ((park.getRequiredPower() - cached.get(forcastIndex).get(park)));
+				try {
+					logger.debug("Issue command in round " + round + " with amount of " + -amount + " for round "
+							+ forcastIndex);
+					scenario.getCommandFactory().createAdjustConsumerCommand(park, -amount).assign();
+				} catch (CannotAssignCommandException e) {
+					e.printStackTrace();
+					throw new RuntimeException("Error in round " + round + " not expected to happen.");
+				}
+			}
+		}
+	}
+
+	/**
+	 * Preprocess maximum provided values for industrial parks for a whole day.
+	 * 
+	 * @param scenario {@link Scenario}
+	 */
+	private void preProcess(Scenario scenario) {
+		List<SolarPowerPlant> plants = scenario.getGraph().getNodes().stream().filter(n -> n instanceof SolarPowerPlant)
+				.map(n -> (SolarPowerPlant) n).collect(Collectors.toList());
+
+		for (int hour = 0; hour < 24; hour++) {
+			Map<Producer, Integer> producerCapacities = new HashMap<>();
+			for (SolarPowerPlant plant : plants) {
+				logger.debug(
+						"Prod capacity in round  " + hour + " " + plant.getMaximumEnergyLevel() * sunIntensity[hour]);
+				producerCapacities.put(plant, (int) (plant.getMaximumEnergyLevel() * sunIntensity[hour]));
+			}
+
+			Map<Consumer, Integer> requiredCap = simulateWith(scenario.getGraph(), producerCapacities);
+
+			StringBuffer logMsg = new StringBuffer();
+			logMsg.append("in hour " + hour + " ");
+			requiredCap.keySet().forEach(c -> {
+				logMsg.append("Consumer " + c.getName() + " requires " + requiredCap.get(c));
+			});
+			logger.debug(logMsg);
+
+			cached.put(hour, requiredCap);
+		}
+	}
+
+	private Map<Consumer, Integer> simulateWith(Graph<EnergyNode, PowerLine> graph,
+			Map<Producer, Integer> producerCapacities) {
+		AbstractEnergyNetworkAnalyzer analyzer = new EnergyNetworkAnalyzerImpl(graph, Optional.of(producerCapacities),
+				Optional.empty());
+		analyzer.calculateMaxFlow();
+		return analyzer.getConsumerLevels();
+	}
+
+	/**
+	 * @see org.sopra.api.exercises.ExerciseSubmission#getTeamIdentifier()
+	 */
+	@Override
+	public String getTeamIdentifier() {
+		return "Musterloesung";
+	}
+
+}
---
+++ Aufgabenblock06/Ausarbeitung/G05T4AB6/src/solutions/exercise6/ScenarioImpl.java
@@ -0,0 +1,367 @@
+package solutions.exercise6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.sopra.api.CostCalculatorFactory;
+import org.sopra.api.Game;
+import org.sopra.api.Multiplicator;
+import org.sopra.api.Scenario;
+import org.sopra.api.command.CannotAssignCommandException;
+import org.sopra.api.command.CannotExecuteCommandException;
+import org.sopra.api.exercises.ExerciseSubmission;
+import solutions.exercise1.ScenarioUtilImpl;
+import solutions.exercise2.ConstructionCostCalculatorImpl;
+import solutions.exercise2.PlayfieldElementComparator;
+
+import org.sopra.api.model.EnergyNode;
+import org.sopra.api.model.PlantLocation;
+import org.sopra.api.model.PlayfieldElement;
+import org.sopra.api.model.PlayfieldElement.ElementType;
+import org.sopra.api.model.PowerLine;
+import org.sopra.api.model.PowerLineType;
+import org.sopra.api.model.TransformerStation;
+import org.sopra.api.model.consumer.Consumer;
+import org.sopra.api.model.consumer.ControllableConsumer;
+import org.sopra.api.model.producer.ControllableProducer;
+import org.sopra.api.model.producer.Producer;
+import org.sopra.api.model.producer.ProducerType;
+
+public class ScenarioImpl implements Game, ExerciseSubmission
+{
+
+	@Override
+	public String getTeamIdentifier()
+	{
+		return "G05T04";
+	}
+
+	@Override
+	public void buildPhase(Scenario scenario)
+	{
+		// TODO Auto-generated method stub
+		ScenarioUtilImpl util = new ScenarioUtilImpl();
+		List<PowerLine> lowLines = util.getPowerLinesByType(scenario.getGraph(), PowerLineType.LOW_VOLTAGE);
+		List<PowerLine> mediumLines = util.getPowerLinesByType(scenario.getGraph(), PowerLineType.MEDIUM_VOLTAGE);
+		List<PowerLine> highLines = util.getPowerLinesByType(scenario.getGraph(), PowerLineType.HIGH_VOLTAGE);
+		List<Consumer> consumers = util.getConsumers(scenario.getGraph());
+		List<ControllableConsumer> controllableConsumers = util.getControllableConsumers(scenario.getGraph());
+		List<Producer> producers = util.getProducers(scenario.getGraph());
+		List<ControllableProducer> controllableProducers = util.getControllableProducers(scenario.getGraph());
+		HashMap<TransformerStation, PlayfieldElement> transformerStations = new HashMap<TransformerStation, PlayfieldElement>();
+		for(EnergyNode node : scenario.getGraph().getNodes())
+		{
+			if(node instanceof TransformerStation)
+			{
+//				System.out.println("TransformerStation: " + node.getName());
+				transformerStations.put((TransformerStation) node, scenario.getPlayfield().getPlayfieldElement(node.getXPos(), node.getYPos()));
+			}
+		}
+		
+		int maximumLoadConsumers = 0;
+		int maximumLoadControllableConsumers = 0;
+		int maximumLoadProducers = 0;
+		int maximumLoadControllableProducers = 0;
+		for(Consumer con : consumers)
+		{
+			System.out.println("Consumer: " + con.getName());
+			maximumLoadConsumers += con.getMaximumEnergyLevel();
+		}
+		for(ControllableConsumer con : controllableConsumers)
+		{
+			System.out.println("ControllableConsumer: " + con.getName());
+			maximumLoadControllableConsumers += con.getMaximumEnergyLevel();
+		}
+		for(Producer prod : producers)
+		{
+			System.out.println("Producer: " + prod.getName());
+			maximumLoadProducers += prod.getMaximumEnergyLevel();
+		}
+		for(ControllableProducer prod : controllableProducers)
+		{
+			System.out.println("ControllableProducer: " + prod.getName());
+			maximumLoadControllableProducers += prod.getMaximumEnergyLevel();
+		}
+		System.out.println(maximumLoadConsumers);
+		System.out.println(maximumLoadControllableConsumers);
+		System.out.println(maximumLoadProducers);
+		System.out.println(maximumLoadControllableProducers);
+		
+		System.out.println("tranformerStations.size() = " + transformerStations.size());
+		ConstructionCostCalculatorImpl calculateCost = new ConstructionCostCalculatorImpl(scenario);
+		if(transformerStations.size() > 0)
+		{
+			System.out.println("Going through all TransformerStation objects in transformerStations HashMap");
+			for(TransformerStation transformerStation : transformerStations.keySet())
+			{
+				double lowestCost = Double.MAX_VALUE;
+				ProducerType lowestCostType = null;
+				System.out.println("Initialisation of lowestCost and lowestCostType succesfull");
+				for(ProducerType type : ProducerType.values())
+				{
+					System.out.println("Going through every ProducerType: " + type.name() + "     ElementType: " + transformerStations.get(transformerStation).getElementType());
+					System.out.println(buildLocationDoesMatchPlantType(transformerStations.get(transformerStation).getElementType(), type));
+					if(buildLocationDoesMatchPlantType(transformerStations.get(transformerStation).getElementType(), type))
+					{
+						System.out.println("ElementType matches ProducerType " + buildLocationDoesMatchPlantType(transformerStations.get(transformerStation).getElementType(), type));
+						if(calculateCost.calculateCost(transformerStations.get(transformerStation), type) < lowestCost)
+						{
+							lowestCost = calculateCost.calculateCost(transformerStations.get(transformerStation), type);
+							lowestCostType = type;
+							System.out.println("Type for lowestCost was found for: " + lowestCostType.name());
+						}
+					}
+				}
+				if(scenario.getStatistics().getOverallScore() + lowestCost < 20000)
+				{
+					System.out.println("Maximum Cost was not exceeded" + scenario.getStatistics().getOverallScore() + " lowestCost: " + lowestCost);
+					PlantLocation buildLocation = null;
+					for(PlantLocation location : scenario.getPlantLocations())
+					{
+						if(transformerStations.get(transformerStation).equals(location.getPlayfieldElement()))
+						{
+							System.out.println("Found build location");
+							buildLocation = location;
+						}
+					}
+					if(buildLocation != null && lowestCostType != null)
+					{
+						try
+						{
+							scenario.getCommandFactory().createBuildPlantCommand(buildLocation, lowestCostType).execute();
+							System.out.println("Build Command executed");
+						}
+						catch (CannotExecuteCommandException e)
+						{
+							
+						}
+					}
+				}
+			}
+		}
+		System.out.println(lowLines.size());
+		if(lowLines.size() != 0)
+		{
+			for(PowerLine line : lowLines)
+			{
+				try
+				{
+					scenario.getCommandFactory().createUpgradeLineCommand(line, PowerLineType.HIGH_VOLTAGE).execute();
+					System.out.println("lowLine upgraded");
+				}
+				catch(Exception e)
+				{
+					
+				}
+			}
+		}
+		System.out.println(mediumLines.size());
+		if(mediumLines.size() != 0)
+		{
+			for(PowerLine line : mediumLines)
+			{
+				try
+				{
+					scenario.getCommandFactory().createUpgradeLineCommand(line, PowerLineType.HIGH_VOLTAGE).execute();
+					System.out.println("meduimLine upgraded");
+				}
+				catch(Exception e)
+				{
+					
+				}
+			}
+		}
+	}
+
+	private boolean buildLocationDoesMatchPlantType(ElementType element, ProducerType type)
+	{
+		if(element == null)
+		{
+			System.out.println("ElementType == null");
+			return false;
+		}
+		else if(type == null)
+		{
+			System.out.println("ProducerType == null");
+			return false;
+		}
+		switch (element)
+		{
+			case BEACH:
+			{
+				switch (type)
+				{
+					case GASFIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case HYDRO_POWER_PLANT:
+					{
+						return true;
+					}
+					case NUCLEAR_POWER_PLANT:
+					{
+						return true;
+					}
+					case SOLAR_POWER_PLANT:
+					{
+						return true;
+					}
+					case WIND_POWER_PLANT:
+					{
+						return true;
+					}
+					default:
+					{
+						return false;
+					}
+				}
+			}
+			case GRASSLAND:
+			{
+				switch (type)
+				{
+					case BIOGAS_FIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case COALFIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case GASFIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case NUCLEAR_POWER_PLANT:
+					{
+						return true;
+					}
+					case SOLAR_POWER_PLANT:
+					{
+						return true;
+					}
+					case WIND_POWER_PLANT:
+					{
+						return true;
+					}
+					default:
+					{
+						return false;
+					}
+				}
+			}
+			case MOUNTAIN:
+			{
+				switch (type)
+				{
+					case COALFIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case GASFIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case SOLAR_POWER_PLANT:
+					{
+						return true;
+					}
+					case WIND_POWER_PLANT:
+					{
+						return true;
+					}
+					default:
+					{
+						return false;
+					}	
+				}
+			}
+			case RIVER:
+			{
+				switch (type)
+				{
+					case BIOGAS_FIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case COALFIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case GASFIRED_POWER_PLANT:
+					{
+						return true;
+					}
+					case HYDRO_POWER_PLANT:
+					{
+						return true;
+					}
+					case NUCLEAR_POWER_PLANT:
+					{
+						return true;
+					}
+					case SOLAR_POWER_PLANT:
+					{
+						return true;
+					}
+					case WIND_POWER_PLANT:
+					{
+						return true;
+					}
+					default:
+					{
+						return false;
+					}
+				}
+			}
+			case SEA:
+			{
+				switch (type)
+				{
+					case HYDRO_POWER_PLANT:
+					{
+						return true;
+					}
+					case SOLAR_POWER_PLANT:
+					{
+						return true;
+					}
+					case WIND_POWER_PLANT:
+					{
+						return true;
+					}
+					default:
+					{
+						return false;
+					}
+				}
+			}
+			default:
+			{
+				return false;
+			}
+		}
+	}
+	
+	@Override
+	public void executionPhase(Scenario scenario, int round)
+	{
+		// TODO Auto-generated method stub
+		ScenarioUtilImpl util = new ScenarioUtilImpl();
+		List<ControllableProducer> controllableProducers = util.getControllableProducers(scenario.getGraph());
+		for(ControllableProducer prod : controllableProducers)
+		{
+			System.out.println("Current Energy:" + prod.getEnergyLevel() + "    Maximum Warp: " + prod.getMaximumEnergyLevel());
+			try
+			{
+				scenario.getCommandFactory().createAdjustProducerCommand((ControllableProducer) prod, prod.getMaximumEnergyLevel() - prod.getEnergyLevel()).assign();
+			}
+			catch (CannotAssignCommandException e)
+			{
+				
+			}
+		}
+	}
+}
